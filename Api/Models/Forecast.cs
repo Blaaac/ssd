fdccf88a6cbfafbc6c0a42dfd3ce5f9baa10e366
Blaac <ruben.ceroni@studio.unibo.it>
2021-01-18 17:08:57 +0100

    fix build err

--- Api/Models/Forecast.cs
+++ Api/Models/Forecast.cs
@@ -14,7 +14,7 @@ namespace Api {
       int timeout = 10000;
       PythonRunner PR = new PythonRunner (interpreter, environment, timeout);
       // Bitmap bmp = null;
-
+      string attribute = indexes[0];
       try {
         string command = $"Models/forecast.py {attribute}.csv";
         string list = PR.runDosCommands (command);
@@ -60,59 +60,59 @@ namespace Api {
         return res;
 
     }
-  }
-  public string forecastSARIMAindex (string attribute) {
-    string res = "\"text\":\"";
-    string interpreter = "/home/ruben/anaconda3/bin/python";
-    string environment = "opanalytics";
-    int timeout = 10000;
-    PythonRunner PR = new PythonRunner (interpreter, environment, timeout);
-    // Bitmap bmp = null;
 
-    try {
-      string command = $"Models/forecastStat.py {attribute}.csv";
-      string list = PR.runDosCommands (command);
-      if (string.IsNullOrWhiteSpace (list)) {
-        Console.WriteLine ("error in script call");
+    public string forecastSARIMAindex (string attribute) {
+      string res = "\"text\":\"";
+      string interpreter = "/home/ruben/anaconda3/bin/python";
+      string environment = "opanalytics";
+      int timeout = 10000;
+      PythonRunner PR = new PythonRunner (interpreter, environment, timeout);
+      // Bitmap bmp = null;
+
+      try {
+        string command = $"Models/forecastStat.py {attribute}.csv";
+        string list = PR.runDosCommands (command);
+        if (string.IsNullOrWhiteSpace (list)) {
+          Console.WriteLine ("error in script call");
+          goto lend;
+        }
+        string[] lines = list.Split (new [] { Environment.NewLine }, StringSplitOptions.None);
+        string strBitMap = "";
+        foreach (string s in lines) {
+          if (s.StartsWith ("MAPE")) {
+            Console.Write (s);
+            res += s;
+          }
+          if (s.StartsWith ("b'")) {
+            Console.WriteLine ("aaaaaaaaaaaaaaaaaaa");
+            strBitMap = s.Trim ();
+            break;
+          }
+          if (s.StartsWith ("Actual")) {
+            double fcast = Convert.ToDouble (s.Substring (s.LastIndexOf (" ")));
+            Console.WriteLine (fcast);
+          }
+        }
+        Console.Write (strBitMap);
+        strBitMap = strBitMap.Substring (strBitMap.IndexOf ("b'"));
+        res += "\",\"img\":\"" + strBitMap + "\"";
+        // try{
+        //   bmp = PR.FromPythonBase64String(strBitMap);
+        // }
+        // catch(Exception e){
+        //   throw new Exception( "error occured while trying to create image: ", e);
+
+        // }
+        goto lend;
+
+      } catch (Exception e) {
+        Console.WriteLine (e.ToString ());
         goto lend;
       }
-      string[] lines = list.Split (new [] { Environment.NewLine }, StringSplitOptions.None);
-      string strBitMap = "";
-      foreach (string s in lines) {
-        if (s.StartsWith ("MAPE")) {
-          Console.Write (s);
-          res += s;
-        }
-        if (s.StartsWith ("b'")) {
-          Console.WriteLine ("aaaaaaaaaaaaaaaaaaa");
-          strBitMap = s.Trim ();
-          break;
-        }
-        if (s.StartsWith ("Actual")) {
-          double fcast = Convert.ToDouble (s.Substring (s.LastIndexOf (" ")));
-          Console.WriteLine (fcast);
-        }
-      }
-      Console.Write (strBitMap);
-      strBitMap = strBitMap.Substring (strBitMap.IndexOf ("b'"));
-      res += "\",\"img\":\"" + strBitMap + "\"";
-      // try{
-      //   bmp = PR.FromPythonBase64String(strBitMap);
-      // }
-      // catch(Exception e){
-      //   throw new Exception( "error occured while trying to create image: ", e);
+      lend:
 
-      // }
-      goto lend;
+        return res;
 
-    } catch (Exception e) {
-      Console.WriteLine (e.ToString ());
-      goto lend;
     }
-    lend:
-
-      return res;
-
   }
-}
 }
